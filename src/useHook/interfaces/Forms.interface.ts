74988bbac23286d3b15206e6d60fc02995a4aa36
Luciano Cardozo Casariego <lucianoc.c117@gmail.com>
2023-09-10 20:44:37 -0300

    init repo

---
+++ src/useHook/interfaces/Forms.interface.ts
@@ -0,0 +1,9 @@
+import { IFormValidations } from './Validator.interface';
+
+export type IFormState = {
+  [key: string]: string | number | boolean;
+};
+
+export type IFormValidateData = {
+  [key: string]: IFormValidations;
+};
